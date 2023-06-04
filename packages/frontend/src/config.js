5928b79fdcbe493640382d6c5ee1c1acb7031ffc
Chance <jchancehud@gmail.com>
2023-06-04 01:53:38 -0500

    chore: update urls

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -2,7 +2,7 @@ import { ethers } from 'ethers'
 
 const prod = NODE_ENV === 'production'
 
-export const SERVER = prod ? 'https://relay.zketh.io' : 'http://localhost:8000'
+export const SERVER = prod ? 'https://http.ceremony.unirep.io' : 'http://localhost:8000'
 export const WS_SERVER = prod
-  ? 'wss://relay.zketh.io/ws'
+  ? 'wss://ws.ceremony.unirep.io'
   : 'ws://localhost:8001'
