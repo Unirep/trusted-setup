f8cb461a1dfcaea36c9efeb250e4f0171b03a661
bhargav <bhargav.annem@gmail.com>
2023-09-22 12:37:39 -0500

    feat: integrated cosmo

--- packages/frontend/src/pages/Contribute.jsx
+++ packages/frontend/src/pages/Contribute.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react-lite'
 import { Link, useLocation } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
+import { useState, useRef } from 'react'
 import 'react-toastify/dist/ReactToastify.css'
 
 import Header from '../components/Header'
@@ -12,6 +13,11 @@ import InfoContainer from '../components/InfoContainer'
 import state from '../contexts/state'
 import './contribute.css'
 
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Galaxy from '../components/Galaxy'
+import init, { CosmoSim } from 'wasm'
+
 const ContributeState = {
   loading: 0,
   normal: 1,
@@ -21,7 +27,44 @@ const ContributeState = {
   offline: 5,
 }
 
+const vertexShaderSrc = `
+  varying vec3 vPosition;
+  varying float vMass;
+
+  attribute float mass;
+
+  void main() {
+    vPosition = position;
+    vMass = mass;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+    gl_PointSize = 1.;
+  }
+`
+
+const fragmentShaderSrc = `
+const float PI = 3.1415926;
+
+varying vec3 vPosition;
+varying float vMass;
+
+void main() {
+    vec3 color = vMass * vec3(236., 46., 0.) + (1. - vMass) * vec3(1., 94., 158.);
+    gl_FragColor = vec4(color / 255., 1.0);
+}
+
+  `
+
 export default observer(() => {
+  const [fmm, setFmm] = useState(null)
+
+  const N_plummer = 1000
+  const N_disk = 200
+  const N_halo = 300
+  const N_bulge = 400
+  const N_spheres = 1
+  const AU = 1e11
+
   const [name, setName] = React.useState('')
   const [error, setError] = React.useState('')
   const [cosmoCanvasReady, setCosmoCanvasReady] = React.useState(false)
@@ -63,25 +106,74 @@ export default observer(() => {
   }, [error])
 
   React.useEffect(() => {
+    init().then(() => {
+      setFmm(
+        new CosmoSim(N_plummer, N_disk, N_bulge, N_halo, AU, N_plummer * 1e24)
+      )
+    })
+  }, [])
+
+  React.useEffect(() => {
     if (cosmoCanvasReady) {
       const width = window.innerWidth * 0.9
       const height = window.innerHeight * 0.5
 
       const canvas = document.getElementById('cosmo')
+      console.log('canvas -- ', canvas)
       canvas.width = width
       canvas.height = height
+      const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 
-      const ctx = canvas.getContext('2d')
-      ctx.font = '2rem Azeret Mono'
-      ctx.fillStyle = 'white'
-      ctx.textAlign = 'center'
-      ctx.fillText(
-        "Bhargov's cosmo should be here.",
-        canvas.width / 2,
-        canvas.height / 2
+      // const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      let camera = new THREE.PerspectiveCamera(35, 1, 0.01, 10000 * AU)
+      camera.position.set(0, 0, 1.7 * AU)
+
+      const scene = new THREE.Scene()
+      let clock = new THREE.Clock()
+
+      const particleGeometry = new THREE.BufferGeometry()
+      let positions = new Float32Array(3 * N_plummer)
+      let mass = new Float32Array(N_plummer).fill(1, 0, N_spheres * N_plummer)
+      particleGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
       )
+      particleGeometry.setAttribute('mass', new THREE.BufferAttribute(mass, 1))
+
+      const particleShader = new THREE.ShaderMaterial({
+        vertexShader: vertexShaderSrc,
+        fragmentShader: fragmentShaderSrc,
+        uniforms: {},
+      })
+
+      const cameraControls = new OrbitControls(camera, renderer.domElement)
+      cameraControls.noPan = false
+
+      const particleSystem = new THREE.Points(particleGeometry, particleShader)
+
+      scene.add(camera)
+      scene.add(particleSystem)
+
+      function render() {
+        var seconds = clock.getDelta()
+        if (seconds > 1) {
+          seconds = 1
+        }
+        const timestep = seconds * 60 * 60 * 24 * 15
+        console.log(`${timestep / 1000} ms`)
+        fmm.simulate(timestep)
+        positions = fmm.get_position()
+
+        const buf = new THREE.BufferAttribute(new Float32Array(positions), 3)
+        particleGeometry.setAttribute('position', buf)
+        particleGeometry.attributes.position.needsUpdate = true
+
+        renderer.render(scene, camera)
+        requestAnimationFrame(render)
+      }
+      requestAnimationFrame(render)
     }
-  }, [cosmoCanvasReady])
+  }, [cosmoCanvasReady, fmm])
 
   const splitContributionText = () => {
     const circuitKeys = ceremony.contributionHashes
@@ -264,7 +356,7 @@ export default observer(() => {
         <div className="content">
           <Header logoOnly={true} />
           <div className="canvas-container">
-            <canvas id="cosmo">Bhargav canvas should be here.</canvas>
+            <canvas id="cosmo"></canvas>
             <p>
               Unirep Multiverse generator. Drop the force & create your own
               verse.
